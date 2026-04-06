6d9ea33f0a15325c42383f8175aeb723beaadd42
saysa <sabyasachi7629@gmail.com>
2026-04-06 20:08:06 +0530

    components

--- frontend/src/api/listingApi.js
+++
@@ -1,7 +0,0 @@
-import api from "./axios";
-
-export const getListings = (params) =>
-  api.get("/listings", { params });
-
-export const applyToListing = (id) =>
-  api.post(`/listing/${id}/apply`);
--- frontend/src/api/recruiterApi.js
+++
@@ -1,7 +0,0 @@
-import api from "./axios";
-
-export const createListing = (data) =>
-  api.post("/listings", data);
-
-export const getRecruiterApplications = (listingId) =>
-  api.get(`/applications/listing/${listingId}`);
