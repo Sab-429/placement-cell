0b613ae9627bddb2d4ea9a30c2c72027f1446e62
saysa <sabyasachi7629@gmail.com>
2026-06-22 21:07:12 +0530

    student profile updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ import ProtectedRoute from './components/ProtectedRoute'
 import StudentDashboard from './pages/student/Dashboard'
 import ListingDetails from './pages/student/ListingDetails'
 import Listings from './pages/student/Listings'
+import StudentProfile from './pages/student/Profile'
 
 export default function App() {
     const { role } = useAuthStore()
@@ -21,10 +22,10 @@ export default function App() {
                 <Route path="/register" element={<Register />} />
 
                 <Route path="/student" element={<ProtectedRoute role="student" />}>
-                <Route path="dashboard"    element={<StudentDashboard />} />
-                <Route path="listings"     element={<Listings />} />
-                <Route path="listings/:id" element={<ListingDetails />} />
-
+                    <Route path="dashboard" element={<StudentDashboard />} />
+                    <Route path="profile" element={<StudentProfile />} />
+                    <Route path="listings" element={<Listings />} />
+                    <Route path="listings/:id" element={<ListingDetails />} />
                 </Route>
                 <Route path="*" element={<Navigate to={home} replace />} />
             </Routes>
