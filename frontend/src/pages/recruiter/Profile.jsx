77ab0b7fa54819f26c0c134ac5112d688d2f9313
saysa <sabyasachi7629@gmail.com>
2026-07-04 21:04:08 +0530

    recruiter create Listings

--- frontend/src/pages/recruiter/Profile.jsx
+++ frontend/src/pages/recruiter/Profile.jsx
@@ -1,11 +1,32 @@
+import client from "@/api/client";
 import useAuthStore from "@/store/authStore";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { domain } from "zod/v4/core/regexes.cjs";
 
 export default function RecruiterProfile() {
     const { userId } = useAuthStore()
-    const [profile, setProfile] = useState(null)
-    const [logoPreview, setLogoPreview] = useState(null)
-    const [savings,setSavings] = useState(false)
+    const [form, setForm] = useState({})
+    const [saved, setSaved] = useState(false)
+    const [loading, setLoading] = useState(false)
+
+    useEffect(() => {
+        client.get(`/recruiters/${userId}`)
+        .then(({data}) => setForm(data))
+    }, [userId])
+
+    const handleSave = async (e) => {
+        e.preventDefault()
+        setLoading(true)
+        await client.put(`/recruiters/${userId}`, {
+            name: form.name, 
+            about: form.about,
+            domain: form.domain,
+            num_employees: ,
+
+        })
+    }
+    
+
 
     
 }
